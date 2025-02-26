6e00ac2b7e114bf2ee0810ab7c0fec45ed334db9
Mregacho29 <mregacho@academic.rrc.ca>
2025-02-25 18:07:50 -0600

    Add activerecord associations to model Pet, Tag, Shelter, Pets_Tag

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,5 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+  has_many :pets_tags
+  has_many :tags, through: :pets_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :pets_tags
+  has many :pets, through: :pets_tags
 end
