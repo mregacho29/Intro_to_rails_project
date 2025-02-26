91e7ba0381ae31e055dba20b1c0daa5f753132ec
Mregacho29 <mregacho@academic.rrc.ca>
2025-02-25 18:08:38 -0600

    Add validations to pet model in pet.rb file

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,13 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   has_many :pets_tags
   has_many :tags, through: :pets_tags
+
+  validates :pet_id, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :breed, presence: true
+  validates :age, presence: true
+  validates :size, presence: true
+  validates :gender, presence: true
+  validates :status, presence: true
 end
